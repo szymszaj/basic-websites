c73a144e3e397e73e16e1b9da5c861c6757586fc
Szymon Zych <szymonzych868@gmail.com>
2023-10-26 17:56:16 +0200

    changing scroll navigation

--- Flexbox website-paradise/js/main.js
+++ Flexbox website-paradise/js/main.js
@@ -12,6 +12,8 @@ const footerYear = document.querySelector('.footer__year')
 const handleNav = () => {
     nav.classList.toggle('nav--active')
 
+    navBtnBars.classList.remove('black-bars-color')
+
     allNavItems.forEach(item => {
         item.addEventListener('click', () => {
             nav.classList.remove('nav--active')
@@ -30,10 +32,26 @@ const handleNavItemsAnimation = () => {
     })
 }
 
+const handleObserver = () => {
+    const currentSection = window.scrollY;
+
+    allSections.forEach(section => {
+        if (section.classList.contains('white-section') &&
+            section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.add('black-bars-color')
+        } else if (!section.classList.contains('white-section') &&
+            section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.remove('black-bars-color')
+        }
+
+    })
+}
 
 const handleCurrentYear = () => {
     const year = (new Date).getFullYear();
     footerYear.innerText = year;
 }
+
 handleCurrentYear()
 navBtn.addEventListener('click', handleNav)
+window.addEventListener('scroll', handleObserver)
