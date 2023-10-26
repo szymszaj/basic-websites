8b16d1536ea0887eca35510f399cfc379b2a2f6d
Szymon Zych <szymonzych868@gmail.com>
2023-10-26 14:34:05 +0200

    Adding a footer from the date

--- Flexbox website-paradise/js/main.js
+++ Flexbox website-paradise/js/main.js
@@ -1,6 +1,13 @@
 const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.burger-btn');
 const allNavItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
+
+
+
+
 
 const handleNav = () => {
     nav.classList.toggle('nav--active')
@@ -22,4 +29,11 @@ const handleNavItemsAnimation = () => {
         delayTime++;
     })
 }
+
+
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+handleCurrentYear()
 navBtn.addEventListener('click', handleNav)
