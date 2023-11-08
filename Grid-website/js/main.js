015cf8a97b6cfec8e6559ee3cee7bb06a2fa4dbe
Szymon Zych <szymonzych868@gmail.com>
2023-11-08 18:04:53 +0100

    support for closing navigation after clicking a button

--- Grid-website/js/main.js
+++ Grid-website/js/main.js
@@ -2,17 +2,33 @@ const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
 
-
 const handleNav = () => {
-    navBtn.classList.toggle('is-active')
-    navMobile.classList.toggle('nav-mobile--active')
+    navBtn.classList.toggle('is-active');
+    navMobile.classList.toggle('nav-mobile--active');
+};
 
-}
-navBtn.addEventListener('click', handleNav)
+navBtn.addEventListener('click', handleNav);
 
 const handleCurrentYear = () => {
-    const year = (new Date).getFullYear();
+    const year = new Date().getFullYear();
     footerYear.innerText = year;
-}
+};
 
-handleCurrentYear()
+handleCurrentYear();
+
+// Pobierz wszystkie linki w nawigacji
+const navLinks = document.querySelectorAll('.nav__link');
+
+// Dodaj obsługę zdarzenia na kliknięcie linku
+navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        // Usuń klasę aktywną z nawigacji, aby ją zamknąć
+        navMobile.classList.remove('nav-mobile--active');
+    });
+});
+
+// Pobierz przycisk zamykający nawigację i dodaj obsługę zdarzenia
+const closeNavButton = document.getElementById('close-nav-button');
+closeNavButton.addEventListener('click', () => {
+    navMobile.classList.remove('nav-mobile--active');
+});
