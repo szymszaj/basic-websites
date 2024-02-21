03fd1a26de1b0e0b2c394e69f1e725e14c50bfd8
Szymon Zych <szymonzych868@gmail.com>
2024-02-21 18:54:05 +0100

    add import

--- page-in-react/src/App.js
+++ page-in-react/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import home from "./components/home";
 
 function App() {
   return (
     <div className="App">
-      
+      <home />
     </div>
   );
 }
