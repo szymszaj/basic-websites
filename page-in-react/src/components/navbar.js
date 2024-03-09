a20f084f15ef8df783f56cce96f8aa3621e7a02f
Szymon Zych <szymonzych868@gmail.com>
2024-03-09 11:08:41 +0100

    creating nav

--- page-in-react/src/components/navbar.js
+++ page-in-react/src/components/navbar.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import navbar from "./navbar";
 import Logo from "../Assets/Logo.svg";
+import { BsCart2 } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import {
+  Box,
+  Drawer,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListIconText,
+} from "@mui/material";
+import HomIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
 
 const navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Home",
+      icon: <HomeIcon />,
+    },
+    {
+      text: "About",
+      icon: <InfoIcon />,
+    },
+    {
+      text: "Testimonials",
+      icon: <CommentRoundedIcon />,
+    },
+    {
+      text: "Contact",
+      icon: <PhoneRoundedIcon />,
+    },
+    {
+      text: "Cart",
+      icon: <ShoppingCartRoundedIcon />,
+    },
+  ];
+
   return <div>navbar</div>;
 };
 
