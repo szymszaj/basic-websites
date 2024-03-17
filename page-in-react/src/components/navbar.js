d2cf8a0ea1e890aca09fdc0a9ae28350916a7bae
Szymon Zych <szymonzych868@gmail.com>
2024-03-17 19:43:59 +0100

    creating website navigation

--- page-in-react/src/components/navbar.js
+++ page-in-react/src/components/navbar.js
@@ -1,23 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
-import navbar from "./navbar";
 import Logo from "../Assets/Logo.svg";
 import { BsCart2 } from "react-icons/bs";
 import { HiOutlineBars3 } from "react-icons/hi2";
-import {
-  Box,
-  Drawer,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListIconText,
-} from "@mui/material";
-import HomIcon from "@mui/icons-material/Home";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import HomeIcon from "@mui/icons-material/Home";
 import InfoIcon from "@mui/icons-material/Info";
 import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
 import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
 import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
 
-const navbar = () => {
+const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const menuOptions = [
     {
@@ -41,8 +41,46 @@ const navbar = () => {
       icon: <ShoppingCartRoundedIcon />,
     },
   ];
-
-  return <div>navbar</div>;
+  return (
+    <nav>
+      <div className="nav-logo-container">
+        <img src={Logo} alt="" />
+      </div>
+      <div className="navbar-links-container">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Testimonials</a>
+        <a href="">Contact</a>
+        <a href="">
+          <BsCart2 className="navbar-cart-icon" />
+        </a>
+        <button className="primary-button">Bookings Now</button>
+      </div>
+      <div className="navbar-menu-container">
+        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+      </div>
+      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={() => setOpenMenu(false)}
+          onKeyDown={() => setOpenMenu(false)}
+        >
+          <List>
+            {menuOptions.map((item) => (
+              <ListItem key={item.text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+        </Box>
+      </Drawer>
+    </nav>
+  );
 };
 
-export default navbar;
+export default Navbar;
